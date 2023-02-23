922618b46c26415540008f8115c77a1f9c2108f1
Aman7860 <amanrce@gmail.com>
2023-02-23 15:02:48 +0530

    New changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateemployeeComponent } from './createemployee/createemployee.component';
@@ -29,8 +29,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatTableModule,
     HttpClientModule,
     ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  //Inline Template
+  // template: `<div>
+  //       <h1> Hello {{localdata}} how are you ? </h1>
+  //       <h2> Welcome to Inline Template ! </h2>
+  //   </div>`,
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
+  localdata: string = "DASHBOARD";
   constructor() { }
-
+  title: string = "";
+  image: string = "";
+  message: string = "";
+  random: string = "";
   ngOnInit(): void {
+    this.title = "Types of Data Binding";
+    this.image = "assets/sponsor-22.png"
+  }
+  onclick() {
+    this.message = "Thank you for subscribe!";
+    console.log("Thank you for subscribe!")
+
   }
 
 }
